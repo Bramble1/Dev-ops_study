73c2d1bcc7083162e889ed5d0827aa125321daa9
Sam D <24442697+digitalXmage@users.noreply.github.com>
2023-06-18 09:55:39 +0100

    Create variables.tf

---
+++ Terraform/lab/variables.tf
@@ -0,0 +1,11 @@
+variable "mybucket"{
+    description="our bucket"
+    type=string
+    default="terraformstate7505e1f0"
+}
+
+variable "mytable"{
+    description="our table"
+    type=string
+    default="terraform-state-lock"
+}
