4541a103aa65cd162fe6752f4ef7abbece7c1696
Sam D <24442697+digitalXmage@users.noreply.github.com>
2023-09-04 12:42:54 +0100

    Update main.tf

--- Terraform/Example/Parent/main.tf
+++ Terraform/Example/Parent/main.tf
@@ -1,1 +1,18 @@
+terraform {
+    required_providers {
+        aws = {
+            source="hashicorp/aws"
+            version="3.7"
+        }
+    }
+}
 
+provider "aws" {
+    region="us-west-2"
+}
+
+module "webserver" {
+    source = "./modules/ec2"
+    servename = "calabvm"
+    instance_size = "t2.micro"
+}
